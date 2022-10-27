647838cba692262e016b4b6c38873c056c2e927d
Nikunj bisht <nikkubisht12@gmail.com>
2022-10-27 09:37:06 +0530

    first commit

--- src/main/java/com/example/interviewkit/controller/QuizController.java
+++ src/main/java/com/example/interviewkit/controller/QuizController.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -64,8 +66,9 @@ QuizQuestionsRepository quizQuestionsRepository;
 
 
     }
+    @GetMapping("/getQuizs")
+    public ResponseEntity getQuizes(@RequestParam(name = "quizId") String quizId , @RequestParam(name = "userId") String userId){
 
-
-
-
+    return new ResponseEntity(null,HttpStatus.OK);
+    }
 }
